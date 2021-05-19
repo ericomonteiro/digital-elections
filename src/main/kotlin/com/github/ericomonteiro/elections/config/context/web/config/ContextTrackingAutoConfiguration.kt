05cba060f968308201248e66d00af1281dab15e6
ericomonteirozup <erico.monteiro@zup.com.br>
2021-05-18 22:05:20 -0300

    remove unnecessary auto config

--- src/main/kotlin/com/github/ericomonteiro/elections/config/context/web/config/ContextTrackingAutoConfiguration.kt
+++
@@ -1,8 +0,0 @@
-package com.github.ericomonteiro.elections.config.context.web.config
-
-import org.springframework.context.annotation.Configuration
-import org.springframework.context.annotation.Import
-
-@Configuration
-@Import(TrackingConfig::class)
-class ContextTrackingAutoConfiguration
